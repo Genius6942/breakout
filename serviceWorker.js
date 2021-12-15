6d8672b14d57c9edc0f8ae56f075458b2c41b8d0
a person <74061612+Genius6942@users.noreply.github.com>
2021-12-15 17:36:44 +0000

    Initial commit

---
+++ serviceWorker.js
@@ -0,0 +1,20 @@
+const thingsToAdd = [
+	'/',
+	'/index.html',
+	'/favicon.ico'
+]
+
+self.addEventListener('install', (e) => {
+	e.waitUntil(
+		caches.open('site').then(cache => {
+			cache.addAll(thingsToAdd);
+		})
+	);
+});
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+	    caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request);
+	    })
+	);
+});
