4a94ec7d5d0541a24a23a3c36fccf03e0b42f6f6
a person <74061612+Genius6942@users.noreply.github.com>
2021-12-15 19:51:45 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,9 +12,14 @@ self.addEventListener('install', (e) => {
 	);
 });
 self.addEventListener("fetch", fetchEvent => {
-	fetchEvent.respondWith(
-	    caches.match(fetchEvent.request).then(res => {
-			return res || fetch(fetchEvent.request);
-	    })
-	);
+	fetchEvent.respondWith((async () => {
+		const r = await caches.match(e.request);
+		console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+		if (r) return r;
+		const response = await fetch(e.request);
+		const cache = await caches.open(cacheName);
+		console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+		cache.put(e.request, response.clone());
+		return response;
+	});
 });
